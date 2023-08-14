e1945fc84c805cc5c96f32de13e43ab63f3947c0
carlosnomade <eujovemdev@gmail.com>
2023-08-14 00:45:58 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -9,19 +9,6 @@ app.use(cors());
 
 const requests = [];
 
-const checkUserId = (request, response, next) => {
-    const { id } = request.params;
-    const index = requests.findIndex(user => user.id === id);
-
-    if (index < 0) {
-        return response.status(404).json({ error: "User not found" });
-    };
-
-    request.userId = requests[index].id;
-    request.index = index;
-
-    next()
-}
 
 app.get('/order', (request, response) => {
     return response.json(requests);
@@ -33,46 +20,6 @@ app.post('/order', (request, response) => {
 
     requests.push(ask);
     return response.status(201).json(ask);
-});
-
-
-app.put('/order/:id', checkUserId, (request, response) => {
-    const { orderUser, clientName } = request.body;
-    const { index } = request;
-    const id = request.userId; 
-
-    const updateRequests = { id, orderUser, clientName};
- 
-    requests[index] = updateRequests;
-    return response.json(updateRequests);
-});
-
-app.delete('/order/:id', checkUserId, (request, response) => {
-    const { index } = request;
-    requests.splice(index, 1);
-
-    return response.status(204).json();
-});
-
-app.get('/order/:id', checkUserId, (request, response) => {
-    const { index } = request;
-    return response.json(requests[index]);
-});
-
-app.patch('/order/:id', checkUserId, (request, response) => {
-    const { index } = request;
-    const id = request.userId; 
-
-    const orderUser  = requests[index].orderUser;
-    const clientName  = requests[index].clientName;
-    const price  = requests[index].price;
-    const status = 'Finalizado';
-
-
-    const updateRequests = { id, orderUser, clientName, price, status };
-
-    requests[index] = updateRequests;
-    return response.json(updateRequests);
 });
 
 
